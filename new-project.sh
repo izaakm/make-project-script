86b6a24759c474223705acb7566d9dd044d1d332
izaakm <izaakm@users.noreply.github.com>
2024-02-23 09:56:31 -0500

    Major overhaul of new-project.sh

--- new-project.sh
+++ new-project.sh
@@ -1,94 +1,116 @@
 #!/usr/bin/env bash
 
-# Don't overwrite files.
+# Don't overwrite files, just to be safe.
 set -o noclobber
 
 # Exit immediately if any command returns an error code.
 set -e
 
 # Exit if no arguments were provided.
-[ $# -eq 0 ] && { echo "Usage: $0 <project>"; exit 1; }
-
-# the first argument passed into the script should be the dir
-# where you want the folder structure setup
-
-echo "Setting up folder structure in $1"
-
-# Do not modify existing directories.
-if [ -d "$1" ]; then
-    echo "[ERROR] Project directory already exists: $1"
-    exit 2
+if [ $# -eq 0 ] ; then
+    echo "Create a new project directory."
+    echo "Usage: $0 <project>"
+    exit 1
 fi
 
+# The first argument passed into the script should be the name of the directory
+# where you want your new project setup.
+project_dir="$1"
+
+echo "Setting up folder structure in ${project_dir}"
+
 # Make the project directory then cd into it.
-mkdir $1
-cd $1
+mkdir "${project_dir}"
+cd "${project_dir}"
 
-# Create an empty README file and initialize this directory as a Git repo.
-touch README.md
-git init .
-git add README.md
-git commit -m "Initialize new project repository"
+# Create subdirectories.
+mkdir bin data docs notebooks results src
 
-# Setup subdirectories.
-mkdir doc data src bin results
+# Create data subdirectories.
+mkdir data/anthro data/derived data/raw 
 
-cd doc
-echo "Doc directory with one subdirectory per manuscript" > README
-touch .gitkeep
+# Ignore some files.
+cat > .gitignore << EOF
+.env
+tmp
+EOF
 
-cd ../data
-echo "Data directory for storing fixed data sets" > README
-touch .gitkeep
+# Create dummy files so that Git will know about these directories. IE, even if
+# you don't want the files in these directories to be tracked by git, you still
+# want to be sure that the same directory structure exists on any/all remotes.
+touch bin/.gitkeep
+touch data/anthro/.gitkeep
+touch data/derived/.gitkeep
+touch data/raw/.gitkeep
+touch docs/.gitkeep
+touch notebooks/.gitkeep
+touch results/.gitkeep
+touch src/.gitkeep
 
-cd ../src
-echo "src for source code" > README
-touch .gitkeep
-
-cd ../bin
-echo "bin for compiled binaries or scripts" > README
-touch .gitkeep
-
-cd ../results
-echo "Results directory for tracking computational experiments peformed on data" > README
-touch .gitkeep
+# Ignore (almost) everything inside the data directory.
+cat > data/.gitignore << EOF
+*
+!.gitignore
+!.gitkeep
+EOF
 
 echo "Folders created."
 
-cd ..
 
+# Create the README file.
 cat > README.md << EOF
-#computational-project-cookie-cutter
-A cookie cutter (aka project template) to set up a folder structure for a computational project.
-This is a quick way to setup a folder structure that follows one standard to organize a project.
-This helps with project management, reproducibility, sharing, and publishing your data, analysis, and results.
+# PROJECT TITLE
 
-This project was inspired (and modeled off) by:
+Description of your project.
 
-[Noble WS 2009 A Quick Guide to Organizing Computational Biology Projects. PLoS Comput Biol 5 7: e1000424. doi:10.1371/journal.pcbi.1000424](http://dx.doi.org/10.1371/journal.pcbi.1000424)
+## Project structure
 
-## What it does
-the \`setup_project_dir.sh\` script creates the following folder structure:
 
-    Path_Provided
-    |- doc/           # directory for documentation, one subdirectory for manuscript
-    |
-    |- data/          # data for storing fixed data sets
-    |
-    |- src/           # any source code
-    |
-    |- bin/           # any compiled binaries or scripts
-    |
-    |- results/       # output for tracking computational experiments performed on data
+    ${project_dir}
+    ├── README.md
+    ├── bin        => Compiled binaries or scripts
+    ├── data       => Immutable data
+    ├── docs       => Documentation of this project
+    ├── notebooks  => Notebooks (e.g., Jupyter, R Markdown)
+    ├── results    => Tables, figures, etc., (i.e., for the *results* section of your manuscript)
+    └── src        => Source code
 
-A README containing a brief blurb is placed in each folder.
-This is because git will not track empty folders and placing a README will
-remind you of what goes in each folder, and also the overall
-folder structure will be retained
 
-If you use a webservice in conjunction with your version control (e.g. github, bitbucket, gitlabs, gitbucket, etc)
-the webservice will be able to render these README and other [markdown](https://help.github.com/articles/markdown-basics/) files automatically.
-
-This project was taken from [this](https://github.com/chendaniely/computational-project-cookie-cutter) github repo
+<!-- END OF README -->
 EOF
+
 echo "Top-level README.md created"
+
+# Create a citation file. For more information about citation files, see:
+#   https://citation-file-format.github.io/
+# Example cff from:
+#   https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-citation-files
+cat > CITATION.cff << EOF
+cff-version: 1.2.0
+title: "My Research Software"
+authors:
+- family-names: "Lisa"
+  given-names: "Mona"
+  orcid: "https://orcid.org/0000-0000-0000-0001"
+- family-names: "Bot"
+  given-names: "Hew"
+  orcid: "https://orcid.org/0000-0000-0000-0002"
+version: 2.0.4
+doi: 10.5281/zenodo.1234
+date-released: 2017-12-18
+url: "https://github.com/github-linguist/linguist"
+message: "If you use this software, please cite it as below."
+EOF
+
+echo "CITATION.cff created"
+
+# Set up the .env file. Use single quotes with `echo` to avoid expanding variables.
+echo 'export PATH="$PATH:$(pwd -P)/bin"' > .env
+
+# Initialize the repo.
+git init .
+git add .
+git commit -m "Initialize new project repository"
+
+# All done!
+exit 0
